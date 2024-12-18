907071067cdaaff64de30094f91b4f37379e529d
mimisarkar <anglemimisarkar@gmail.com>
2024-12-18 12:54:16 +0600

    18/12/24

---
+++ app.js
@@ -0,0 +1,55 @@
+// let a=5
+// let b=9
+
+
+
+// let cal = (a*b/0.5)
+// console.log(cal);
+
+
+
+
+
+// let r=9
+
+
+// let cal = 
+
+
+
+
+
+
+
+
+// let d = 24//h
+// let h = 60//m
+// let m = 60//sec
+// let hm = (h*m)*d*day
+// console.log(hm)
+
+
+
+
+// let day = prompt(" day:")
+// document.getElementById("aaa")innertext = "kurigram"
+
+
+
+// let text = prompt("text:")
+// document.getElementById("aaa").innerText = text 
+
+
+
+
+
+
+let  num_1 = prompt("num 1:")
+     num_1= parseInt(num_1)
+
+let num_2 = prompt("num 2:")
+    num_2 = parseFloat(num_2)
+
+
+   let text = num_1+ num_2
+   document.getElementById("aaa").innerText=text
